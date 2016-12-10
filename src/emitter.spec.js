a1efd195450d70235f28f76ed2bd004d04d769fc
Justin Krueger <justin@kroo.gs>
2016-12-10 01:28:58 -0700

    Added circle.yml to configure node version, updated test and bumped version.

--- src/emitter.spec.js
+++ src/emitter.spec.js
@@ -1,35 +1,34 @@
 import { expect } from 'chai';
-import Emitter from '../src/emitter';
+import Emitter from './emitter';
 
-describe('Emitter', () => {
+describe('Emitter', function () {
   beforeEach(function () {
     this.emitter = new Emitter();
   });
 
   it('#on should register a callback for an event', function () {
-    function func() {}
+    function fn() {}
 
-    this.emitter.on('event', func);
+    this.emitter.on('event', fn);
 
     expect(this.emitter._events.event.size).to.equal(1);
   });
 
   it('#off should unregister a callback for an event', function () {
-    function func() {}
+    function fn() {}
 
-    this.emitter.on('event', func);
-    this.emitter.off('event', func);
+    this.emitter.on('event', fn);
+    this.emitter.off('event', fn);
 
     expect(this.emitter._events.event.size).to.equal(0);
   });
 
   it('#off should unregister all callbacks for an event', function () {
-    function func() {}
-    function func2() {}
+    function fn() {}
+    function fn2() {}
 
-    this.emitter.on('event', func);
-    this.emitter.on('event', func2);
-
+    this.emitter.on('event', fn);
+    this.emitter.on('event', fn2);
     this.emitter.off('event');
 
     expect(this.emitter._events.event.size).to.equal(0);
@@ -38,11 +37,11 @@ describe('Emitter', () => {
   it('#emit should run all callbacks for an event with supplied argument', function () {
     const data = [];
 
-    function func(input) { data.push(input); }
-    function func2(input) { data.push(input); }
+    function fn(input) { data.push(input); }
+    function fn2(input) { data.push(input); }
 
-    this.emitter.on('event', func);
-    this.emitter.on('event', func2);
+    this.emitter.on('event', fn);
+    this.emitter.on('event', fn2);
     this.emitter.emit('event', 'ran');
 
     expect(data).to.deep.equal(['ran', 'ran']);
