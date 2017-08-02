051af6852b1ab6c6c40fb7e15b14969f3bcbb22f
Justin Krueger <justin@kroo.gs>
2017-08-01 22:38:09 -0700

    Integrate flow type checking.

--- src/emitter.js
+++ src/emitter.js
@@ -1,26 +1,28 @@
 import { EmitterError } from './error'
 
+type callback = (input: mixed) => void
+
 export default class Emitter {
   constructor() {
     this._events = {}
   }
 
-  _event(name) {
+  _event(name: string): Set {
     return (this._events[name] = this._events[name] || new Set())
   }
 
-  on(name, fn) {
+  on(name: string, fn: callback): void {
     if (typeof fn !== 'function') throw new EmitterError('requires function')
     this._event(name).add(fn)
   }
 
-  off(name, fn) {
+  off(name: string, fn: callback): void {
     if (!this._events[name]) return
     if (fn) this._event(name).delete(fn)
     else this._event(name).clear()
   }
 
-  once(name, fn) {
+  once(name: string, fn: callback): void {
     const self = this
     this.on(name, function(input) {
       self.off(name, this)
@@ -28,7 +30,7 @@ export default class Emitter {
     })
   }
 
-  emit(name, input) {
+  emit(name: string, input: mixed): void {
     if (!this._events[name]) return
     this._event(name).forEach(fn => fn(input))
   }
