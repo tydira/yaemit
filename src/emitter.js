0e105f32b829a7786fb860d4d6c5761b3a9980da
Justin Krueger <justin@kroo.gs>
2017-08-01 21:28:22 -0700

    Check for fn in .on(), bail early in .off()

--- src/emitter.js
+++ src/emitter.js
@@ -1,3 +1,5 @@
+import { EmitterError } from './error'
+
 export default class Emitter {
   constructor() {
     this._events = {}
@@ -8,10 +10,12 @@ export default class Emitter {
   }
 
   on(name, fn) {
+    if (typeof fn !== 'function') throw new EmitterError('requires function')
     this._event(name).add(fn)
   }
 
   off(name, fn) {
+    if (!this._events[name]) return
     if (fn) this._event(name).delete(fn)
     else this._event(name).clear()
   }
