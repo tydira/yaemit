40e2bf6c3540fb23ae348a5f885a60c7540f1307
Justin Krueger <justin@kroo.gs>
2017-08-04 12:34:22 -0700

    Improve test readability and organization.

--- src/emitter.test.js
+++ src/emitter.test.js
@@ -5,67 +5,64 @@ describe('Emitter', function() {
     this.emitter = new Emitter()
   })
 
-  it('#on should register a callback for an event', () => {
-    function fn() {}
+  describe('#on', () => {
+    it('registers a callback for an event', () => {
+      function fn() {}
+      this.emitter.on('event', fn)
+      expect(this.emitter._eventMap.event.values().next().value).toBe(fn)
+    })
 
-    this.emitter.on('event', fn)
-
-    expect(this.emitter._events.event.size).toBe(1)
+    it('throws an TypeError if called without a function', () => {
+      expect(() => this.emitter.on('event')).toThrowError(TypeError)
+      expect(() => {
+        this.emitter.on('event', 100)
+      }).toThrowError('requires callback')
+    })
   })
 
-  it('#on should throw an EmitterError if called without a function', () => {
-    expect(() => this.emitter.on('event')).toThrowError(TypeError)
-    expect(() => {
-      this.emitter.on('event', 100)
-    }).toThrowError('requires callback')
+  describe('#off', () => {
+    it('unregisters a callback for an event', () => {
+      function fn() {}
+
+      this.emitter.on('event', fn)
+      this.emitter.off('event', fn)
+
+      expect(this.emitter._eventMap.event.size).toBe(0)
+    })
+
+    it('unregisters all callbacks for an event', () => {
+      function fn() {}
+      function fn2() {}
+
+      this.emitter.on('event', fn)
+      this.emitter.on('event', fn2)
+      this.emitter.off('event')
+
+      expect(this.emitter._eventMap.event.size).toBe(0)
+    })
+
+    it('returns quickly if unable to find event', () => {
+      this.emitter.off('event')
+      expect(this.emitter._eventMap.event).toBe(undefined)
+    })
   })
 
-  it('#off should unregister a callback for an event', () => {
-    function fn() {}
+  describe('#emit', () => {
+    it('runs all callbacks for an event with the supplied argument', () => {
+      const data = []
+      const fn = input => data.push(input)
+      const fn2 = input => data.push(input)
 
-    this.emitter.on('event', fn)
-    this.emitter.off('event', fn)
+      this.emitter.on('event', fn)
+      this.emitter.on('event', fn2)
+      this.emitter.emit('event', 'ran')
 
-    expect(this.emitter._events.event.size).toBe(0)
-  })
+      expect(data).toEqual(['ran', 'ran'])
+    })
 
-  it('#off should unregister all callbacks for an event', () => {
-    function fn() {}
-    function fn2() {}
-
-    this.emitter.on('event', fn)
-    this.emitter.on('event', fn2)
-    this.emitter.off('event')
-
-    expect(this.emitter._events.event.size).toBe(0)
-  })
-
-  it('#off should return quickly if an event is undefined', () => {
-    this.emitter.off('event')
-
-    expect(this.emitter._events.event).toBe(undefined)
-  })
-
-  it('#emit should run all callbacks for an event with the supplied argument', () => {
-    const data = []
-
-    function fn(input) {
-      data.push(input)
-    }
-    function fn2(input) {
-      data.push(input)
-    }
-
-    this.emitter.on('event', fn)
-    this.emitter.on('event', fn2)
-    this.emitter.emit('event', 'ran')
-
-    expect(data).toEqual(['ran', 'ran'])
-  })
-
-  it('#emit should return quickly if an event is undefined', () => {
-    this.emitter.emit('event')
-
-    expect(this.emitter._events.event).toBe(undefined)
+    it('returns quickly if unable to find event', () => {
+      this.emitter.emit('event')
+      expect(this.emitter._eventMap.event).toBe(undefined)
+    })
   })
 })
